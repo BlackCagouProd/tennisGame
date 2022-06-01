d5395777d9c54abe3396d3ad8aba5cd17456a20b
BlackCagouProd <blackcagou@outlook.fr>
2022-06-02 01:48:41 +0200

    add function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //creation des variables (parametrage du jeux )
 //les points sont dans un tableau ,le match et le match config sont des objets
+//utilisation de jquery
 
 
 const point = [0, 15, 30, 40, "AD"];
@@ -34,6 +35,9 @@ for (
 
 );
 
+/**------------------------------------------------fonctions------------------------------------------------------------------- */
+
+
 //fonction permaettant de preparer le xcore du set
 function Scores() {
     Points();
@@ -54,36 +58,161 @@ function Scores() {
 //fonction permettant de preparer le point du jeu 
 function Points() {
     if (!match.tieBreakActive[match.currentSet]) {
-        fadeHtml("#p1points", point[match.points[0]]);
-        fadeHtml("#p2points", point[match.points[1]]);
+        fadeHtml("#Bobpoints", point[match.points[0]]);
+        fadeHtml("#Annapoints", point[match.points[1]]);
     } else {
         fadeHtml(
-            "#p1points",
+            "#Bobpoints",
             match.tieBreakScore[match.currentSet][0]
         );
         fadeHtml(
-            "#p2points",
+            "#Annapoints",
             match.tieBreakScore[match.currentSet][1]
         );
     }
 }
 
-var scoreBob = document.getElementById('#scoreBob');
-var scoreAnna = document.getElementById("#scoreAnna")
 
-scoreBob.click(function() {
+$("#scroreBob").click(function() {
 
     playerScores(0);
-    drawScores();
-
+    Scores();;
 });
 
-scoreAnna.click(function() {
+$("#scroreAnna").click(function() {
 
     playerScores(1);
-    drawScores();
-
+    Scores();
 
 });
-console.table(match.games.push([0, 0]) <= 5);
+//fonction permettant le scorage du jeux
+function playerScores(winningPlayer) {
+    match.latestEvent.level = 1;
+
+    var losingPlayer = (winningPlayer + 1) % 2;
+
+    if (match.tieBreakActive[match.currentSet]) {
+        match.tieBreakScore[match.currentSet][winningPlayer]++;
+        if (
+            match.tieBreakScore[match.currentSet][winningPlayer] >=
+            matchConfig.tieBreakPoints &&
+            match.tieBreakScore[match.currentSet][winningPlayer] -
+            match.tieBreakScore[match.currentSet][losingPlayer] >=
+            2
+        ) {
+            playerWinsSet(winningPlayer);
+        }
+    } else {
+        // 0 to 30
+        if (match.points[winningPlayer] <= 2)
+            match.points[winningPlayer]++;
+        // 40
+        else if (match.points[winningPlayer] == 3) {
+            // l = 40 - both had 40
+            if (match.points[losingPlayer] == 3)
+                match.points[winningPlayer]++;
+            // l = AD
+            else if (match.points[losingPlayer] == 4)
+                match.points[losingPlayer]--;
+            else playerWinsGame(winningPlayer);
+        }
+        // AD
+        else if (match.points[winningPlayer] == 4)
+            playerWinsGame(winningPlayer);
+    }
+    if (match.latestEvent.level <= 1)
+        match.latestEvent.text =
+        matchConfig.players[winningPlayer] + " marque le point";
+
+    // transmit the match 
+    emitMatch();
+}
+//fonctions du paramettrage du tiebreak
+function drawGamesInSet(set, showTieBreakScore) {
+    fadeHtml(
+        "#Bobset" + parseInt(set + 1),
+        match.games[set][0] +
+        (showTieBreakScore ?
+            "<sup>" +
+            match.tieBreakScore[match.currentSet][0] +
+            "</sup>" :
+            "")
+    );
+    fadeHtml(
+        "#Annaset" + parseInt(set + 1),
+        match.games[set][1] +
+        (showTieBreakScore ?
+            "<sup>" +
+            match.tieBreakScore[match.currentSet][1] +
+            "</sup>" :
+            "")
+    );
+}
+
+function playerWinsGame(winningPlayer) {
+    var losingPlayer = (winningPlayer + 1) % 2;
+
+    match.points[0] = 0;
+    match.points[1] = 0;
+
+    match.games[match.currentSet][winningPlayer]++;
+
+    if (
+        match.games[match.currentSet][winningPlayer] >=
+        matchConfig.gamesPerSet
+    ) {
+        // oponent will have at least 2 games difference
+        if (
+            match.games[match.currentSet][winningPlayer] -
+            match.games[match.currentSet][losingPlayer] >=
+            2
+        )
+            playerWinsSet(winningPlayer);
+        else if (
+            match.games[match.currentSet][winningPlayer] ==
+            matchConfig.gamesPerSet &&
+            match.games[match.currentSet][losingPlayer] ==
+            matchConfig.gamesPerSet
+        )
+            match.tieBreakActive[match.currentSet] = true;
+    }
+
+    if (match.latestEvent.level <= 2) {
+        match.latestEvent.level = 2;
+        match.latestEvent.text =
+            matchConfig.players[winningPlayer] + " gagne la partie";
+    }
+}
+
+function playerWinsSet(winningPlayer) {
+    match.playerSetsWon[winningPlayer]++;
+    match.wonSets.push(
+        "#p" +
+        parseInt(winningPlayer + 1) +
+        "set" +
+        parseInt(match.currentSet + 1)
+    );
+    if (match.playerSetsWon[winningPlayer] == (matchConfig.sets + 1) / 2) {
+        match.winner = winningPlayer;
+        return;
+    }
+    match.currentSet++;
+    if (match.latestEvent.level <= 3) {
+        match.latestEvent.level = 3;
+        match.latestEvent.text =
+            matchConfig.players[winningPlayer] + " prend le set ";
+    }
+}
+
+//fonctions de pramattrage du gagnant 
+function drawWinner() {
+    match.latestEvent.level = 4;
+    match.latestEvent.text =
+        matchConfig.players[match.winner] + " gagne le match";
+    $("#scroreBob").prop("disabled", true);
+    $("#scroreAnna").prop("disabled", true);
+}
+
+
+console.table(match.games);
 console.table(match)
